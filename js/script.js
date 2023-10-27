80d73a248fdcc1b600f0e1e55335a068673b749c
Carlos <carlosdiazgirol@hotmail.com>
2023-10-27 15:26:25 +0200

    change script

--- js/script.js
+++ js/script.js
@@ -1,17 +1,1 @@
  // Aquí tu código
-document.addEventListener('DOMContentLoaded', function () {
-  const lista = document.getElementById('lista');
-  const botonAgregar = document.getElementById('agregar');
-
-  // Agregar un elemento a la lista
-  function agregarElemento() {
-      const nuevoElemento = prompt('Ingrese un nuevo elemento:');
-      if (nuevoElemento) {
-          const li = document.createElement('li');
-          li.textContent = nuevoElemento;
-          lista.appendChild(li);
-      }
-  }
-
-  botonAgregar.addEventListener('click', agregarElemento);
-});
