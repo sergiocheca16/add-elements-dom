886cea09c3947bfe508e52973bfd0a5daf2bdfce
Sergio <you@example.com>
2024-11-05 19:36:15 +0100

    listacompra add

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
  // Aquí tu código
+
+
+const lista = document.getElementById("lista");
+const agregar = document.getElementById("agregar");
+
+agregar.addEventListener("click", function() {
+    let listaAsk = prompt("Añade alimentos que deseas comprar");
+
+    if (listaAsk) {
+        const li = document.createElement('li');
+        li.textContent = listaAsk;
+        lista.appendChild(li);
+    }
+});
